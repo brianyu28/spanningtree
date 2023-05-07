c6bb84c01819bf32478eb95a017c7b18273f0e87
Brian Yu <brian@brianyu.me>
2023-05-07 01:25:50 -0400

    Updates

---
+++ _next/static/7p2wUHWEIL4UwKOxTeIoS/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/61-6ecc4ca443215f4d.js","static/chunks/pages/index-09b0cdf2de885e0d.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
